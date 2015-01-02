598e0b3a190fb165b96dec94d06dc78a1f1eecf2
Takuya ASADA <syuu@dokukino.com>
2015-01-03 08:43:03 +0900

    Prepare to vmxon

--- pico.c
+++ pico.c
@@ -1,3 +1,11 @@
+/*
+ * picovmm
+ *
+ * Copyright (C) 2006 Qumranet, Inc.
+ * Copyright (C) 2015 Takuya ASADA
+ *
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -5,10 +13,31 @@
 #include <linux/mm.h>
 #include <linux/fs.h>
 #include <asm/processor.h>
+#include <asm/processor-flags.h>
+#include <asm/msr-index.h>
 
 MODULE_AUTHOR("Takuya ASADA");
 MODULE_DESCRIPTION("tiny VMM implementation");
 MODULE_LICENSE("GPL");
+
+struct vmcs {
+	u32 revision_id;
+	u32 abort;
+	char data[0];
+};
+
+static int size, order;
+static u32 revision_id;
+
+static inline void vmxon(u64 paddr)
+{
+	asm volatile ("vmxon %0" : : "m"(paddr) : "memory", "cc");
+}
+
+static inline void vmxoff(void)
+{
+	asm volatile ("vmxoff" : : : "cc");
+}
 
 static int pico_dev_open(struct inode *inode, struct file *filp)
 {
@@ -61,23 +90,36 @@ static int pico_init(void)
 {
 	int err = 0;
 	unsigned long ecx;
-	u64 msr;
+	u64 msrl;
+	u32 msr_low, msr_high;
 
 	ecx = cpuid_ecx(1);
 	if (test_bit(5, &ecx)) {
 		printk(KERN_ERR "pico: VT-x is not supported\n");
 		return -EOPNOTSUPP;
 	}
-	rdmsrl(MSR_IA32_FEATURE_CONTROL, msr);
-	if ((msr & 5) == 1) {
+
+	rdmsrl(MSR_IA32_FEATURE_CONTROL, msrl);
+	if ((msrl & 5) == 1) {
 		printk(KERN_ERR "pico: VT-x disabled\n");
 		return -EOPNOTSUPP;
+	} else if ((msrl & 5) == 0) {
+		wrmsrl(MSR_IA32_FEATURE_CONTROL, msrl | 5);
 	}
+
+	write_cr4(read_cr4() | X86_CR4_VMXE);
+
+	rdmsr(MSR_IA32_VMX_BASIC, msr_low, msr_high);
+	size = msr_high & 0x1ffff;
+	order = get_order(size);
+	revision_id = msr_low;
+
 	err = misc_register(&pico_dev);
 	if (err) {
 		printk(KERN_ERR "pico: cannot register device\n");
 		return err;
 	}
+
 	return 0;
 }
 
