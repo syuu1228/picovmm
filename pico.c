b2e344bed1457021f5db86676dd46e8210126d47
Takuya ASADA <syuu@dokukino.com>
2015-01-04 04:19:23 +0900

    Implement vmentry, vmcs_read, vmcs_write ioctls

--- pico.c
+++ pico.c
@@ -28,13 +28,16 @@
 #include <asm/processor.h>
 #include <asm/processor-flags.h>
 #include <asm/msr-index.h>
+#include <asm/uaccess.h>
 #include "pico.h"
+#include "vmx.h"
 
 MODULE_AUTHOR("Takuya ASADA");
 MODULE_DESCRIPTION("KVM like tiny VMM implementation for an education");
 MODULE_LICENSE("GPL");
 
 #define MINOR_COUNT 1
+#define NR_BAD_MSRS 2
 
 struct vmcs {
 	u32 revision_id;
@@ -42,12 +45,72 @@ struct vmcs {
 	char data[0];
 };
 
+enum {
+	VCPU_REGS_RAX = 0,
+	VCPU_REGS_RCX = 1,
+	VCPU_REGS_RDX = 2,
+	VCPU_REGS_RBX = 3,
+	VCPU_REGS_RSP = 4,
+	VCPU_REGS_RBP = 5,
+	VCPU_REGS_RSI = 6,
+	VCPU_REGS_RDI = 7,
+	VCPU_REGS_R8 = 8,
+	VCPU_REGS_R9 = 9,
+	VCPU_REGS_R10 = 10,
+	VCPU_REGS_R11 = 11,
+	VCPU_REGS_R12 = 12,
+	VCPU_REGS_R13 = 13,
+	VCPU_REGS_R14 = 14,
+	VCPU_REGS_R15 = 15,
+	NR_VCPU_REGS
+};
+
+struct vcpu_state {
+	int   launched;
+	unsigned long regs[NR_VCPU_REGS]; /* for rsp: vcpu_load_rsp_rip() */
+	unsigned long rip;      /* needs vcpu_load_rsp_rip() */
+
+	unsigned long cr2;
+	unsigned long cr3;
+	unsigned long cr8;
+};
+
+struct descriptor_table {
+	u16 limit;
+	unsigned long base;
+} __attribute__((packed));
+
+struct segment_descriptor {
+	u16 limit_low;
+	u16 base_low;
+	u8  base_mid;
+	u8  type : 4;
+	u8  system : 1;
+	u8  dpl : 2;
+	u8  present : 1;
+	u8  limit_high : 4;
+	u8  avl : 1;
+	u8  long_mode : 1;
+	u8  default_op : 1;
+	u8  granularity : 1;
+	u8  base_high;
+} __attribute__((packed));
+
+// LDT or TSS descriptor in the GDT. 16 bytes.
+struct segment_descriptor_64 {
+	struct segment_descriptor s;
+	u32 base_higher;
+	u32 pad_zero;
+};
+
 static dev_t dev_id;
 static struct cdev c_dev;
 static struct class *cl;
 static int vmcs_size, vmcs_order;
 static u32 vmcs_revision_id;
-static struct vmcs *vmcs;
+static struct vmcs *vmcs0;
+static struct vcpu_state __vcpu0 = {0};
+static struct vcpu_state *vcpu0 = &__vcpu0;
 
 static inline void vmxon(struct vmcs *vmcs)
 {
@@ -61,7 +124,20 @@ static inline void vmxoff(void)
 	asm volatile ("vmxoff" : : : "cc");
 }
 
-static inline void vmclear(struct vmcs *vmcs)
+static inline int vmptrld(struct vmcs *vmcs)
+{
+	u64 phys_addr = __pa(vmcs);
+	u8 error;
+
+	asm volatile ("vmptrld %1; setna %0"
+		       : "=m"(error) : "m"(phys_addr) : "cc" );
+	if (error)
+		printk(KERN_ERR "kvm: vmptrld %p/%llx fail\n",
+		       vmcs, phys_addr);
+	return error ? -1 : 0;
+}
+
+static inline int vmclear(struct vmcs *vmcs)
 {
 	u64 phys_addr = __pa(vmcs);
 	u8 error;
@@ -71,6 +147,135 @@ static inline void vmclear(struct vmcs *vmcs)
 	if (error)
 		printk(KERN_ERR "kvm: vmclear fail: %p/%llx\n",
 		       vmcs, phys_addr);
+	return error ? -1 : 0;
+}
+
+static inline unsigned long vmcs_readl(unsigned long field);
+static inline int vmcs_read(unsigned long field, unsigned long *valuep)
+{
+	unsigned long value;
+	u8 error;
+
+	asm volatile ("vmread %2, %1; setna %0"
+			: "=g"(error), "=g"(value) : "r"(field) : "cc");
+	if (error)
+		printk(KERN_ERR "vmread error: reg %lx (err %d)\n",
+		       field, (u32)vmcs_readl(VM_INSTRUCTION_ERROR));
+
+	*valuep = value;
+	return error ? -1 : 0;
+}
+
+static inline int vmcs_write(unsigned long field, unsigned long value)
+{
+	u8 error;
+
+	asm volatile ("vmwrite %1, %2; setna %0"
+		       : "=g"(error) : "r"(value), "r"(field) : "cc" );
+	if (error)
+		printk(KERN_ERR "vmwrite error: reg %lx value %lx (err %d)\n",
+		       field, value, (u32)vmcs_readl(VM_INSTRUCTION_ERROR));
+	return error ? -1 : 0;
+}
+
+static inline unsigned long vmcs_readl(unsigned long field)
+{
+	unsigned long value;
+	vmcs_read(field, &value);
+	return value;
+}
+
+static inline u16 vmcs_read16(unsigned long field)
+{
+	return vmcs_readl(field);
+}
+
+static inline u32 vmcs_read32(unsigned long field)
+{
+	return vmcs_readl(field);
+}
+
+static inline u64 vmcs_read64(unsigned long field)
+{
+	return vmcs_readl(field);
+}
+
+static inline void vmcs_writel(unsigned long field, unsigned long value)
+{
+	vmcs_write(field, value);
+}
+
+static inline void vmcs_write16(unsigned long field, u16 value)
+{
+	vmcs_writel(field, value);
+}
+
+static inline void vmcs_write32(unsigned long field, u32 value)
+{
+	vmcs_writel(field, value);
+}
+
+static inline void get_gdt(struct descriptor_table *table)
+{
+	asm ("sgdt %0" : "=m"(*table));
+}
+
+static inline u16 read_fs(void)
+{
+	u16 seg;
+	asm ("mov %%fs, %0" : "=g"(seg));
+	return seg;
+}
+
+static inline u16 read_gs(void)
+{
+	u16 seg;
+	asm ("mov %%gs, %0" : "=g"(seg));
+	return seg;
+}
+
+static inline u16 read_ldt(void)
+{
+	u16 ldt;
+	asm ("sldt %0" : "=g"(ldt));
+	return ldt;
+}
+
+static inline void load_fs(u16 sel)
+{
+	asm ("mov %0, %%fs" : : "g"(sel));
+}
+
+static inline void load_gs(u16 sel)
+{
+	asm ("mov %0, %%gs" : : "g"(sel));
+}
+
+static inline void load_ldt(u16 sel)
+{
+	asm ("lldt %0" : : "g"(sel));
+}
+
+static inline unsigned long read_msr(unsigned long msr)
+{
+	u64 value;
+
+	rdmsrl(msr, value);
+	return value;
+}
+
+static void reload_tss(void)
+{
+	/*
+	 * VT restores TR but not its size.  Useless.
+	 */
+	struct descriptor_table gdt;
+	struct segment_descriptor *descs;
+
+	get_gdt(&gdt);
+	descs = (void *)gdt.base;
+	descs[GDT_ENTRY_TSS].type = 9; /* available TSS */
+	load_TR_desc();
 }
 
 static int pico_dev_open(struct inode *inode, struct file *filp)
@@ -83,10 +288,178 @@ static int pico_dev_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+static int pico_dev_ioctl_vmentry(struct vcpu_state *vcpu)
+{
+	u8 fail;
+	u16 fs_sel, gs_sel, ldt_sel;
+	int fs_gs_ldt_reload_needed;
+
+	/*
+	 * Set host fs and gs selectors.  Unfortunately, 22.2.3 does not
+	 * allow segment selectors with cpl > 0 or ti == 1.
+	 */
+	fs_sel = read_fs();
+	gs_sel = read_gs();
+	ldt_sel = read_ldt();
+	fs_gs_ldt_reload_needed = (fs_sel & 7) | (gs_sel & 7) | ldt_sel;
+	if (!fs_gs_ldt_reload_needed) {
+		vmcs_write16(HOST_FS_SELECTOR, fs_sel);
+		vmcs_write16(HOST_GS_SELECTOR, gs_sel);
+	} else {
+		vmcs_write16(HOST_FS_SELECTOR, 0);
+		vmcs_write16(HOST_GS_SELECTOR, 0);
+	}
+
+	vmcs_writel(HOST_FS_BASE, read_msr(MSR_FS_BASE));
+	vmcs_writel(HOST_GS_BASE, read_msr(MSR_GS_BASE));
+
+	asm (
+		/* Store host registers */
+		"pushf \n\t"
+		"push %%rax; push %%rbx; push %%rdx;"
+		"push %%rsi; push %%rdi; push %%rbp;"
+		"push %%r8;  push %%r9;  push %%r10; push %%r11;"
+		"push %%r12; push %%r13; push %%r14; push %%r15;"
+		"push %%rcx \n\t"
+		"vmwrite %%rsp, %2 \n\t"
+		/* Check if vmlaunch of vmresume is needed */
+		"cmp $0, %1 \n\t"
+		/* Load guest registers.  Don't clobber flags. */
+		"mov %c[cr2](%3), %%rax \n\t"
+		"mov %%rax, %%cr2 \n\t"
+		"mov %c[rax](%3), %%rax \n\t"
+		"mov %c[rbx](%3), %%rbx \n\t"
+		"mov %c[rdx](%3), %%rdx \n\t"
+		"mov %c[rsi](%3), %%rsi \n\t"
+		"mov %c[rdi](%3), %%rdi \n\t"
+		"mov %c[rbp](%3), %%rbp \n\t"
+		"mov %c[r8](%3),  %%r8  \n\t"
+		"mov %c[r9](%3),  %%r9  \n\t"
+		"mov %c[r10](%3), %%r10 \n\t"
+		"mov %c[r11](%3), %%r11 \n\t"
+		"mov %c[r12](%3), %%r12 \n\t"
+		"mov %c[r13](%3), %%r13 \n\t"
+		"mov %c[r14](%3), %%r14 \n\t"
+		"mov %c[r15](%3), %%r15 \n\t"
+		"mov %c[rcx](%3), %%rcx \n\t" /* kills %3 (rcx) */
+		/* Enter guest mode */
+		"jne launched \n\t"
+		"vmlaunch \n\t"
+		"jmp pico_vmx_return \n\t"
+		"launched: vmresume \n\t"
+		".globl pico_vmx_return \n\t"
+		"pico_vmx_return: "
+		/* Save guest registers, load host registers, keep flags */
+		"xchg %3,     0(%%rsp) \n\t"
+		"mov %%rax, %c[rax](%3) \n\t"
+		"mov %%rbx, %c[rbx](%3) \n\t"
+		"pushq 0(%%rsp); popq %c[rcx](%3) \n\t"
+		"mov %%rdx, %c[rdx](%3) \n\t"
+		"mov %%rsi, %c[rsi](%3) \n\t"
+		"mov %%rdi, %c[rdi](%3) \n\t"
+		"mov %%rbp, %c[rbp](%3) \n\t"
+		"mov %%r8,  %c[r8](%3) \n\t"
+		"mov %%r9,  %c[r9](%3) \n\t"
+		"mov %%r10, %c[r10](%3) \n\t"
+		"mov %%r11, %c[r11](%3) \n\t"
+		"mov %%r12, %c[r12](%3) \n\t"
+		"mov %%r13, %c[r13](%3) \n\t"
+		"mov %%r14, %c[r14](%3) \n\t"
+		"mov %%r15, %c[r15](%3) \n\t"
+		"mov %%cr2, %%rax   \n\t"
+		"mov %%rax, %c[cr2](%3) \n\t"
+		"mov 0(%%rsp), %3 \n\t"
+
+		"pop  %%rcx; pop  %%r15; pop  %%r14; pop  %%r13; pop  %%r12;"
+		"pop  %%r11; pop  %%r10; pop  %%r9;  pop  %%r8;"
+		"pop  %%rbp; pop  %%rdi; pop  %%rsi;"
+		"pop  %%rdx; pop  %%rbx; pop  %%rax \n\t"
+		"setbe %0 \n\t"
+		"popf \n\t"
+	      : "=g" (fail)
+	      : "r"(vcpu->launched), "r"((unsigned long)HOST_RSP),
+		"c"(vcpu),
+		[rax]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_RAX])),
+		[rbx]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_RBX])),
+		[rcx]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_RCX])),
+		[rdx]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_RDX])),
+		[rsi]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_RSI])),
+		[rdi]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_RDI])),
+		[rbp]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_RBP])),
+		[r8 ]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R8 ])),
+		[r9 ]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R9 ])),
+		[r10]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R10])),
+		[r11]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R11])),
+		[r12]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R12])),
+		[r13]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R13])),
+		[r14]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R14])),
+		[r15]"i"(offsetof(struct vcpu_state, regs[VCPU_REGS_R15])),
+		[cr2]"i"(offsetof(struct vcpu_state, cr2))
+	      : "cc", "memory" );
+
+	if (!fail) {
+		if (fs_gs_ldt_reload_needed) {
+			load_ldt(ldt_sel);
+			load_fs(fs_sel);
+			/*
+			 * If we have to reload gs, we must take care to
+			 * preserve our gs base.
+			 */
+			local_irq_disable();
+			load_gs(gs_sel);
+			wrmsrl(MSR_GS_BASE, vmcs_readl(HOST_GS_BASE));
+			local_irq_enable();
+
+			reload_tss();
+		}
+		vcpu->launched = 1;
+	}
+	put_cpu();
+	return fail ? -1 : 0;
+}
+
 static long pico_dev_ioctl(struct file *filp,
 			  unsigned int ioctl, unsigned long arg)
 {
-	return 0;
+	long r = -EINVAL;
+
+	switch (ioctl) {
+	case PICO_VMENTRY:
+		r = pico_dev_ioctl_vmentry(vcpu0);
+		break;
+	case PICO_VMCS_READ: {
+		struct pico_vmcs_arg vmcs_arg;
+
+		r = -EFAULT;
+		if (copy_from_user(&vmcs_arg, (void *)arg, sizeof vmcs_arg))
+			goto out;
+		r = vmcs_read(vmcs_arg.field, &vmcs_arg.value);
+		if (r)
+			goto out;
+		r = -EFAULT;
+		if (copy_to_user((void *)arg, &vmcs_arg, sizeof vmcs_arg))
+			goto out;
+		r = 0;
+		break;
+	}
+	case PICO_VMCS_WRITE: {
+		struct pico_vmcs_arg vmcs_arg;
+
+		r = -EFAULT;
+		if (copy_from_user(&vmcs_arg, (void *)arg, sizeof vmcs_arg))
+			goto out;
+		r = vmcs_write(vmcs_arg.field, vmcs_arg.value);
+		if (r)
+			goto out;
+		r = -EFAULT;
+		if (copy_to_user((void *)arg, &vmcs_arg, sizeof vmcs_arg))
+			goto out;
+		r = 0;
+		break;
+	}
+	}
+out:
+	return r;
 }
 
 static int pico_dev_fault(struct vm_area_struct *vma,
@@ -138,26 +511,27 @@ static __init void load_vmx_basic_msr(void)
 	vmcs_revision_id = vmx_msr_low;
 };
 
-static __init int alloc_vmcs(void)
+static __init struct vmcs *alloc_vmcs(void)
 {
 	int node = cpu_to_node(smp_processor_id());
 	struct page *pages;
+	struct vmcs *vmcs;
 
 	pages = alloc_pages_node(node, GFP_KERNEL, vmcs_order);
 	if (!pages)
-		return -1;
+		return NULL;
 	vmcs = page_address(pages);
 	memset(vmcs, 0, vmcs_size);
 	vmcs->revision_id = vmcs_revision_id; /* vmcs revision id */
-	return 0;
+	return vmcs;
 }
 
-static void free_vmcs(void)
+static void free_vmcs(struct vmcs *vmcs)
 {
 	free_pages((unsigned long)vmcs, vmcs_order);
 }
 
-static __init void vmx_enable(void)
+static __init void vmx_enable(struct vmcs *vmcs)
 {
 	u64 old;
 
@@ -191,12 +565,13 @@ static __init int pico_init(void)
 
 	load_vmx_basic_msr();
 
-	if (alloc_vmcs()) {
+	vmcs0 = alloc_vmcs();
+	if (!vmcs0) {
 		printk(KERN_ERR "pico: page allocation failed\n");
 		goto err_vmcs;
 	}
 
-	vmx_enable();
+	vmx_enable(vmcs0);
 
 	r = alloc_chrdev_region(&dev_id, 0, MINOR_COUNT, "pico");
 	if (r < 0) {
@@ -238,7 +613,7 @@ err_class:
 err_chrdev:
 	vmx_disable();
 err_vmcs:
-	free_vmcs();
+	free_vmcs(vmcs0);
 	return -1;
 }
 
@@ -249,7 +624,7 @@ static __exit void pico_exit(void)
 	class_destroy(cl);
 	unregister_chrdev_region(dev_id, MINOR_COUNT);
 	vmx_disable();
-	free_vmcs();
+	free_vmcs(vmcs0);
 	printk(KERN_INFO "pico is unloaded\n");
 }
 
--- pico.h
+++ pico.h
@@ -4,54 +4,13 @@
 #include <asm/types.h>
 #include <linux/ioctl.h>
 
-/* for PICO_RUN */
-struct pico_run {
-	/* in */
-	__u32 vcpu;
-	__u32 emulated;  /* skip current instruction */
-	__u32 mmio_completed; /* mmio request completed */
-
-	/* out */
-	__u32 exit_type;
-	__u32 exit_reason;
-	__u32 instruction_length;
-	union {
-		/* KVM_EXIT_UNKNOWN */
-		struct {
-			__u32 hardware_exit_reason;
-		} hw;
-		/* KVM_EXIT_EXCEPTION */
-		struct {
-			__u32 exception;
-			__u32 error_code;
-		} ex;
-		/* KVM_EXIT_IO */
-		struct {
-#define PICO_EXIT_IO_IN  0
-#define PICO_EXIT_IO_OUT 1
-			__u8 direction;
-			__u8 size; /* bytes */
-			__u8 string;
-			__u8 string_down;
-			__u8 rep;
-			__u8 pad;
-			__u16 port;
-			__u64 count;
-			union {
-				__u64 address;
-				__u32 value;
-			};
-		} io;
-		/* PICO_EXIT_MMIO */
-		struct {
-			__u64 phys_addr;
-			__u8  data[8];
-			__u32 len;
-			__u8  is_write;
-		} mmio;
-	};
+struct pico_vmcs_arg {
+	unsigned long field;
+	unsigned long value;
 };
 
 #define PICO_MAGIC		'p'
-#define PICO_RUN		_IOWR(PICO_MAGIC, 0, struct pico_run)
+#define PICO_VMENTRY		_IO(PICO_MAGIC, 0)
+#define PICO_VMCS_READ		_IOWR(PICO_MAGIC, 1, struct pico_vmcs_arg)
+#define PICO_VMCS_WRITE		_IOWR(PICO_MAGIC, 2, struct pico_vmcs_arg)
 #endif
